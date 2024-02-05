2f2ae3e0989f49d4b950e4247c6b22cccb27778c
HOME-PC\Bulat <bfarukshin@gmail.com>
2024-02-05 15:15:09 +0300

    * thailwind

--- nuxt-demo/layouts/default.vue
+++ nuxt-demo/layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <nuxt-link to="/">Nuxt app</nuxt-link>
-        <ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <nuxt-link to="/" class="font-bold">Nuxt app</nuxt-link>
+        <ul class="flex gap-4">
           <li><nuxt-link to="/">home</nuxt-link></li>
           <li><nuxt-link to="/about">about</nuxt-link></li>
-          <li><nuxt-link to="/products">products</nuxt-link></li>
+          <li><nuxt-link to="/products" class="btn">products</nuxt-link></li>
         </ul>
       </nav>
     </header>
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
   </div>
--- nuxt-demo/layouts/products.vue
+++ nuxt-demo/layouts/products.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <nuxt-link to="/">Nuxt app</nuxt-link>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <nuxt-link to="/" class="font-bold">Nuxt app</nuxt-link>
       </nav>
     </header>
 
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
 
-    <footer>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
       <nav>
         <nuxt-link to="/">Nuxt app</nuxt-link>
-        <ul>
+        <ul class="flex gap-4">
           <li><nuxt-link to="/">home</nuxt-link></li>
           <li><nuxt-link to="/about">about</nuxt-link></li>
           <li><nuxt-link to="/products">products</nuxt-link></li>
